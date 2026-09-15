c43a36dcb4e421b2e843ecf98edd00826e02bf7f
bongohorse <gptbongo@e.mail.de>
2026-09-15 10:43:14 +0200

    fix: render missiles from frozen launch origin (#237)

--- src/entities/GeneratedHazardPresentation.ts
+++ src/entities/GeneratedHazardPresentation.ts
@@ -5,6 +5,7 @@ import {
   type LogicalHazardSpawnInstance,
 } from '../generation/PatternSpawnScheduler';
 import {
+  getPrototypeMissileLaunchScreenLeft,
   getTelegraphedHazardLifecycle,
   type TelegraphedHazardSimulationState,
 } from '../hazards/TelegraphedHazardSimulation';
@@ -50,6 +51,7 @@ export class GeneratedHazardPresentation {
         playerScreenX,
         getTelegraphedHazardLifecycle(telegraphedHazards, spawn),
         verticalProjection,
+        getPrototypeMissileLaunchScreenLeft(telegraphedHazards, spawn),
       );
     }
 
