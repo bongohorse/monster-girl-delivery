e64e6c38c4d18fef3d88d5bcf7ba2d6b281ef70d
bongohorse <gptbongo@e.mail.de>
2026-09-15 10:54:04 +0200

    refactor: pass missile relative launch offset to presentation (#237)

--- src/entities/GeneratedHazardPresentation.ts
+++ src/entities/GeneratedHazardPresentation.ts
@@ -5,7 +5,7 @@ import {
   type LogicalHazardSpawnInstance,
 } from '../generation/PatternSpawnScheduler';
 import {
-  getPrototypeMissileLaunchScreenLeft,
+  getPrototypeMissileLaunchRelativeLeft,
   getTelegraphedHazardLifecycle,
   type TelegraphedHazardSimulationState,
 } from '../hazards/TelegraphedHazardSimulation';
@@ -51,7 +51,7 @@ export class GeneratedHazardPresentation {
         playerScreenX,
         getTelegraphedHazardLifecycle(telegraphedHazards, spawn),
         verticalProjection,
-        getPrototypeMissileLaunchScreenLeft(telegraphedHazards, spawn),
+        getPrototypeMissileLaunchRelativeLeft(telegraphedHazards, spawn),
       );
     }
 
