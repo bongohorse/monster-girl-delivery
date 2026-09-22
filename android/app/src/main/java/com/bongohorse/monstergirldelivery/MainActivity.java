3ce1f9d6668f2012fdf0986107a563fb97e3b1c3
bongohorse <gptbongo@e.mail.de>
2026-09-22 18:31:53 +0200

    fix(android): enable immersive fullscreen

--- android/app/src/main/java/com/bongohorse/monstergirldelivery/MainActivity.java
+++ android/app/src/main/java/com/bongohorse/monstergirldelivery/MainActivity.java
@@ -1,5 +1,45 @@
 package com.bongohorse.monstergirldelivery;
 
+import android.os.Bundle;
+
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        applyImmersiveFullscreen();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        applyImmersiveFullscreen();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            applyImmersiveFullscreen();
+        }
+    }
+
+    private void applyImmersiveFullscreen() {
+        WindowInsetsControllerCompat insetsController =
+                WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
+        if (insetsController == null) {
+            return;
+        }
+
+        insetsController.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
+        insetsController.hide(WindowInsetsCompat.Type.systemBars());
+    }
+}
