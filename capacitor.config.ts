20f1ffb46b627465d0f2cf3100bbd81cef40988c
bongohorse <gptbongo@e.mail.de>
2026-09-22 15:22:08 +0200

    feat: identify and style Android WebView runtime

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,10 @@ const config: CapacitorConfig = {
   appId: 'com.bongohorse.monstergirldelivery',
   appName: 'MGD',
   webDir: 'dist',
+  android: {
+    appendUserAgent: 'MGD-Capacitor/1',
+    backgroundColor: '#121426',
+  },
 };
 
 export default config;
