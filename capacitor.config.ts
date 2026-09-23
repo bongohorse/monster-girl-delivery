77defaaac91db0784b7676213634bfcb6b984480
bongohorse <gptbongo@e.mail.de>
2026-09-23 19:07:06 +0200

    perf(330): label after benchmark app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,11 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.bongohorse.monstergirldelivery',
-  appName: 'MGD',
+  appId: 'com.bongohorse.monstergirldelivery.perf330after',
+  appName: 'MGD 330 AFTER',
   webDir: 'dist',
   android: {
-    appendUserAgent: 'MGD-Capacitor/1',
+    appendUserAgent: 'MGD-Capacitor/1 MGD-Perf330-AFTER/1',
     backgroundColor: '#121426',
   },
 };
